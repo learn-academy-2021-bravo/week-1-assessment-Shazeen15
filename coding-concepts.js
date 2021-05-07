82e6d9e332f3c1de76ab2ec7d7793710b3a13335
Shazeen15 <shazzy.sterlin@gmail.com>
2021-05-07 15:08:42 -0700

    completed the coding-concepts file

--- coding-concepts.js
+++ coding-concepts.js
@@ -7,45 +7,41 @@
 
 // --------------------1) What will this log?
 
-var cohort = "Bravo 2021"
-// console.log(cohort.length)
+var cohort = "Bravo 2021";
+// console.log(cohort.length);
 
-// a) Your answer:
-// b) Verify and explain:
-
+// a) Your answer: 10
+// b) Verify and explain: 10 because the string has 10 characters including the space
 
 // --------------------2) What will this log?
 
-var greeting = "Hello World!"
-// console.log(greeting[3])
+var greeting = "Hello World!";
+// console.log(greeting[3]);
 
-// a) Your answer:
-// b) Verify and explain:
-
+// a) Your answer:l
+// b) Verify and explain:l because l is at the 3rd index in the string
 
 // --------------------3) What will this log?
 
-var languages = ["JavaScript", "Ruby", "Python", "C++"]
-var index = 1
-// console.log(languages[index])
+var languages = ["JavaScript", "Ruby", "Python", "C++"];
+var index = 1;
+// console.log(languages[index]);
 
-// a) Your answer:
-// b) Verify and explain:
-
+// a) Your answer: "Ruby"
+// b) Verify and explain:"Ruby" because "Ruby" is at the 1st index in the array
 
 // --------------------4) What will this log?
 
-var weekendDays = ["saturday", "sunday"]
-// console.log(weekendDays.toUpperCase())
+var weekendDays = ["saturday", "sunday"];
+// console.log(weekendDays.toUpperCase());
 
-// a) Your answer:
-// b) Verify and explain:
-
+// a) Your answer: i dont think anything will happen.
+// b) Verify and explain: error/bug because .toUpperCase() is a string method and not an array method
 
 // --------------------5) What will this log?
 
-var dataTypes = ["number", "string", "Boolean"]
-// console.log(typeof dataTypes.length)
+var dataTypes = ["number", "string", "Boolean"];
+console.log(typeof dataTypes.length);
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: int
+// b) Verify and explain: number because the lenght of the array returns a number/inter
