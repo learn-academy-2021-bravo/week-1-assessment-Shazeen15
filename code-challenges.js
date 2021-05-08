93ce912355a8459d0b4e5f70fdd87c98d56c3f4b
Shazeen15 <shazzy.sterlin@gmail.com>
2021-05-07 17:01:48 -0700

    completed up to task 3

--- code-challenges.js
+++ code-challenges.js
@@ -9,46 +9,121 @@
 // --------------------1) Create a function that takes in a number and determines if a temperature is below boiling point, at boiling point or above boiling point. Boiling point is 212 degrees Fahrenheit.
 // Use the test variables provided below. Expected output: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-var temp1 = 35
-var temp2 = 350
-var temp3 = 212
+var temp1 = 35;
+var temp2 = 350;
+var temp3 = 212;
 
+// const boilingPoint = (temp) => {
+//   if (temp === 212) {
+//     return `${temp} is at boiling point`;
+//   } else if (temp > 212) {
+//     return `${temp} is above boiling point`;
+//   } else if (temp < 212) {
+//     return `${temp} is below boiling point`;
+//   } else {
+//     return `${temp} not valid`;
+//   }
+// };
 
-
-
+// console.log(boilingPoint(temp1));
+// console.log(boilingPoint(temp2));
+// console.log(boilingPoint(temp3));
 
 // --------------------2) Create a function that takes in an array of numbers and returns an array with each number multiplied by 5.
 // Use the test variable provided below. Expected output: [15, 35, 0, 30, -45]
 
-var myNumbers1 = [3, 7, 0, 6, -9]
+// var myNumbers1 = [3, 7, 0, 6, -9];
+// const multBy5 = (numarray) => {
+//   let byFive = [];
+//   for (let i = 0; i < numarray.length; i++) {
+//     byFive.push(numarray[i] * 5);
+//   }
+//   return byFive;
+// };
 
-
-
-
+// console.log(multBy5(myNumbers1));
 
 // --------------------3) Create a function that takes in an array of numbers and returns an array containing only the odd numbers.
 // Use the test variable provided below. Expected output: [-7, 9, 13]
 
-var myNumbers2 = [8, -7, 0, 6, 2, 9, 13]
-
-
-
-
+// var myNumbers2 = [8, -7, 0, 6, 2, 9, 13];
+// const isOdd = (numarray) => {
+//   let oddNums = [];
+//   for (let i = 0; i < myNumbers2.length; i++) {
+//     if (numarray[i] % 2 !== 0) {
+//       oddNums.push(numarray[i]);
+//     }
+//   }
+//   return oddNums;
+// };
+// console.log(isOdd(myNumbers2));
 
 // --------------------4) Create a function that removes all the vowels from a string.
 // Use the test variables provided below. Expected output: "HyThrLrnStdnt" "LvJvScrpt"
 
-var stringWithVowels1 = "HeyThereLearnStudent"
-var stringWithVowels2 = "ILoveJavaScript"
+var stringWithVowels1 = "HeyThereLearnStudent";
+var stringWithVowels2 = "ILoveJavaScript";
+// const vowels = ["a", "e", "i", "o", "u"];
 
+// const noVowels = (str, array) => {
+//   let lowerCaseStr = str.toLowerCase();
+//   let noVow = "";
+//   for (let i = 0; i < lowerCaseStr.length; i++) {
+//     for (let j = 0; j < array.length; j++) {
+//       if (lowerCaseStr[i] !== array[j]) {
+//         // noVow += lowerCaseStr[i];
+//         console.log(array[j]);
+//       }
+//     }
+//   }
+//   return noVow;
+// };
 
-
-
+// const noVowels = (str) => {
+//   let lowerCaseStr = str.toLowerCase();
+//   let noVow = "";
+//   for (let i = 0; i < lowerCaseStr.length; i++) {
+//     if (
+//       lowerCaseStr[i] !== "a" &&
+//       lowerCaseStr[i] !== "e" &&
+//       lowerCaseStr[i] !== "i" &&
+//       lowerCaseStr[i] !== "o" &&
+//       lowerCaseStr[i] !== "u"
+//     ) {
+//       noVow += lowerCaseStr[i];
+//     }
+//   }
+//   return noVow;
+// };
+// console.log(noVowels(stringWithVowels1));
+// console.log(noVowels(stringWithVowels1, vowels));
 
 // --------------------5) Copy/paste your code from #4. Refactor your code to include non-string edge cases. Inform your user if the variable passed into the vowel removal function is not a string.
 // Use the test variables provided below. Expected output: "true is not a string" "42 is not a string" "mCdngMstr"
 // Hint: There is an operator in JavaScript called typeof :)
 
-var refactorTester1 = true
-var refactorTester2 = 42
-var refactorTester3 = "IAmACodingMaster"
+var refactorTester1 = true;
+var refactorTester2 = 42;
+var refactorTester3 = "IAmACodingMaster";
+// console.log(typeof refactorTester3);
+
+const noVowels = (str) => {
+  let lowerCaseStr = typeof str === string ? str.toLowerCase() : str;
+  let noVow = "";
+  for (let i = 0; i < lowerCaseStr.length; i++) {
+    if (
+      lowerCaseStr[i] !== "a" &&
+      lowerCaseStr[i] !== "e" &&
+      lowerCaseStr[i] !== "i" &&
+      lowerCaseStr[i] !== "o" &&
+      lowerCaseStr[i] !== "u"
+    ) {
+      noVow += lowerCaseStr[i];
+    } else {
+      return `not a string`;
+    }
+  }
+  return noVow;
+};
+console.log(noVowels(stringWithVowels1));
+console.log(noVowels(refactorTester1));
